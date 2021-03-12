c3fa63df0ecb003f2a7f10de335a260fcc6b4cfc
Lawrence Freeman <lawrencefreeman@kubrickgroup.com>
2021-03-12 14:08:06 +0000

    first push up of project flasky2

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+#home endpoint
+@app.route('/')
+def home():
+    return jsonify(data='My MLE02 Home Page - Welcome!!!!')
+
+if __name__ == '__main__':
+    # basically saying if this is being importe then the app wont run
+    # only run the app if this script is being executed directly
+    app.run(host='0.0.0.0', port=5000)
